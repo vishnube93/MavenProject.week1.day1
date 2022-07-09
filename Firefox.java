25f199e43756b6a47466f25b63f1d3207d6f1d01
vishnube93 <108991775+vishnube93@users.noreply.github.com>
2022-07-09 21:33:26 +0530

    Add files via upload

---
+++ Firefox.java
@@ -0,0 +1,17 @@
+package week1.day1;
+
+public class Firefox {
+	public static void main(String[] args) {
+		float version = 100.2f;
+		String name = "firefox";
+		boolean visible = true;
+		int year = 1998;
+		char logo = 'm';
+		System.out.println("This is my first script");
+		System.out.println("Version = " + version);
+		System.out.println("Name = " + name);
+		System.out.println("Visible = " + visible);
+		System.out.println("Year = " + year);
+		System.out.println("Logo = " + logo);
+	}
+}
---
+++ Mobile.java
@@ -0,0 +1,16 @@
+package week1.day1;
+
+public class Mobile {
+
+	public static void makeCall() {
+		System.out.println("It can make Calls");
+	}
+	public static void sendMsg() {
+		System.out.println("It can send Messages");
+	}
+	public static void main(String[] args) {
+		System.out.println("This is my mobile");
+		makeCall();
+		sendMsg();
+	}
+}
